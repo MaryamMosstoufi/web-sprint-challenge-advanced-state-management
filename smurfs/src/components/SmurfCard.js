f6e0a4d6c9d9df4c4d1ee6da05217c858bc9abf6
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-17 14:34:37 -0400

    Delete Button

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -4,10 +4,11 @@ import { connect } from 'react-redux';
 const SmurfCard = (props) => {
   return (
     <div className='uk-padding-small'>
-      <div className='uk-card uk-card-primary uk-card-body uk-width-medium'>
+      <div className='uk-card uk-card-primary uk-card-body uk-width-medium uk-visible-toggle'>
         Name: {props.smurf.name} <br />
         Age: {props.smurf.age} <br />
         Height: {props.smurf.height}
+        <a className='uk-float-right uk-hidden-hover' onClick={props.deleteSmurf} data-uk-icon="icon: trash"></a>
       </div>
     </div>
   )
