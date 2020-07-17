100cf01dfe6a3539cdc5c39c755f9cd50e3bbec8
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-17 11:48:07 -0400

    Page Setup

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className='uk-section uk-section-small uk-flex uk-flex-center'>
+        <img src='logo512.png' alt='logo' className='uk-width-small'/>
       </div>
     );
   }
